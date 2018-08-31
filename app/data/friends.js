08d184912757941e233a7c18bc8cd2a4b7d94a4a
JaredWalker <jaredstaires@gmail.com>
2018-08-31 18:54:34 -0500

    edit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,6 @@
 var friendsArray = [
     {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
+      
     }
   ];
   
